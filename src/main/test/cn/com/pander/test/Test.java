dcc9fee0aaaf2025486bfb9b5e33b0f63af7ef4c
fly <fly@fly-pc>
2018-06-08 15:52:04 +0800

    epoll测试

--- src/main/test/cn/com/pander/test/Test.java
+++ src/main/test/cn/com/pander/test/Test.java
@@ -1,10 +1,43 @@
 package cn.com.pander.test;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.spi.SelectorProvider;
+import java.util.Iterator;
 import java.util.concurrent.CountDownLatch;
 
 import cn.com.pander.zookeeper.ZookeeperClient;
 
 public class Test {
+	
+	public void init() throws IOException 
+	{
+		ServerSocketChannel channel=ServerSocketChannel.open();
+		Selector selector=SelectorProvider.provider().openSelector();
+		InetSocketAddress address=new InetSocketAddress(8090);
+		channel.socket().bind(address);
+		channel.register(selector,SelectionKey.OP_ACCEPT );
+		while(true) 
+		{
+			int n=selector.select();
+			if(n>0) 
+			{
+				Iterator<SelectionKey>ite= selector.selectedKeys().iterator();
+				while(ite.hasNext()) 
+				{
+					SelectionKey k=ite.next();
+//					k.channel().
+					ite.remove();
+				}
+			}
+		}
+	}
+	
+	
 	public static void main(String[] args) throws InterruptedException {
 		CountDownLatch latch=new CountDownLatch(100);
 		for(int i=0;i<100;i++) 
